596f38ec9b74dec11fe0044852322d8384ec3193
Anastasios Arvanitis <62649615+AnastasiosArvanitis@users.noreply.github.com>
2021-02-03 13:18:03 +0100

    adding weather service

---
+++ src/app/services/weather.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+  url = 'https://api.openweathermap.org/data/2.5/weather';
+  apiKey = '49f54c932e83c5ed2a5b688235933831';
+
+  constructor(private http: HttpClient) { }
+
+  getWeatherDataByCords(lat, lon): any {
+    const params = new HttpParams()
+      .set('lat', lat)
+      .set('lon', lon)
+      .set('units', 'metric')
+      .set('appid', this.apiKey);
+
+    return this.http.get(this.url, { params });
+  }
+
+  getWeatherDataByCityName(cityName: string): any {
+    const params = new HttpParams()
+      .set('q', cityName)
+      .set('units', 'metric')
+      .set('appid', this.apiKey);
+
+    return this.http.get(this.url, { params });
+  }
+
+}
