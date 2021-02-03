ad0a6dd8d1d7f8edce45600013692205cfea65e8
Anastasios Arvanitis <62649615+AnastasiosArvanitis@users.noreply.github.com>
2021-02-03 13:20:19 +0100

    Weather model object

---
+++ src/app/models/Weather.ts
@@ -0,0 +1,32 @@
+export class Weather {
+  coord: {
+    lon: number,
+    lat: number
+  };
+  sys: {
+    country: string,
+    sunrise: number,
+    sunset: number
+  };
+  main: {
+    temp: number,
+    feels_like: number,
+    temp_min: number,
+    temp_max: number,
+    pressure: number,
+    humidity: number
+  };
+  wind: {
+    speed: number,
+    deg: number
+  };
+  weather: [
+    {
+      id: number,
+      main: string,
+      description: string,
+      icon: string
+    }
+  ];
+  name: string;
+}
